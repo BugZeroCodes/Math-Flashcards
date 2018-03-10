acb2239813ddf66542224cd5ce582b1165b3f058
BugZeroCodes <timothy@timothys-air.attlocal.net>
2018-03-10 12:47:41 -0500

    Rechange after merge mess

--- assets/js/operations.js
+++ assets/js/operations.js
@@ -3,11 +3,7 @@ function getDifficulty() {
   return queryString;
 }
 $('#play-button').click(function(clicked) {
-<<<<<<< HEAD
   var difficulty = getDifficulty();
   var operations = [];
   $('input:checked').each;
-=======
-
->>>>>>> 25d881617bf244120e6d09eb1e3f804dbd1314f3
 });
